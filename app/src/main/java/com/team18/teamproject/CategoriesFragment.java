3b01af1fa7df65f7aa339b19684086c962ab2fee
Alex Bridges <a.m.bridges@ncl.ac.uk>
2016-02-15 12:15:20 +0000

    Copied and renamed java/xml pairs
    Java/XML pairs for categories & favourites pages, set height method
    removed from recent.

--- app/src/main/java/com/team18/teamproject/CategoriesFragment.java
+++ app/src/main/java/com/team18/teamproject/CategoriesFragment.java
@@ -1,16 +1,16 @@
 package com.team18.teamproject;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 
-public class RecentFragment extends Fragment {
+public class CategoriesFragment extends Fragment {
 
-    public RecentFragment() {
+    public CategoriesFragment() {
 
     }
 
@@ -21,7 +21,7 @@ public class RecentFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recent, container, false);
+        return inflater.inflate(R.layout.fragment_categories, container, false);
     }
 
     @Override
