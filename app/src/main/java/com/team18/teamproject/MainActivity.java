b9bb6a01713743f9e49622a11e91ad57d4bd91d8
elasticdonut <dcoutts95@gmail.com>
2016-03-26 22:02:27 +0000

    Fix draw resizing issue and update build tools to latest version
    NOTE: You may have to download the latest build tools in the SDK
    manager.

--- app/src/main/java/com/team18/teamproject/MainActivity.java
+++ app/src/main/java/com/team18/teamproject/MainActivity.java
@@ -1,16 +1,20 @@
 package com.team18.teamproject;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Daniel on 06/12/2015.
  */
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     public Toolbar toolbar;
     private FragmentManager fragmentManager;
@@ -24,10 +28,14 @@ public class MainActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        DrawerFragment drawerFragment = (DrawerFragment) getSupportFragmentManager().findFragmentById(R.id.drawerFragment);
-        drawerFragment.setUp(R.id.drawerFragment, (DrawerLayout) findViewById(R.id.drawerLayout), toolbar);
+//        DrawerFragment drawerFragment = (DrawerFragment) getSupportFragmentManager().findFragmentById(R.id.drawerFragment);
+//        drawerFragment.setUp(R.id.drawerFragment, (DrawerLayout) findViewById(R.id.drawerLayout), toolbar);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
 
-       HomeFragment homeFragment = new HomeFragment();
+        HomeFragment homeFragment = new HomeFragment();
         fragmentManager = getSupportFragmentManager();
         setMainFragment(homeFragment);
     }
@@ -36,5 +44,13 @@ public class MainActivity extends AppCompatActivity{
         fragmentManager.beginTransaction()
                 .replace(R.id.mainFragmentContainer, fragment)
                 .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
