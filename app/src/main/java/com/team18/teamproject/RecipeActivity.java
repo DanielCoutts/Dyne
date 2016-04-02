00359417c50399de60db8d9f7d73d96b137a5c8a
elasticdonut <dcoutts95@gmail.com>
2016-04-02 15:01:26 +0100

    Added back button to action bar in recipe activity
    Note: Could be improved by remembering the position in the previous
    activity.

--- app/src/main/java/com/team18/teamproject/RecipeActivity.java
+++ app/src/main/java/com/team18/teamproject/RecipeActivity.java
@@ -1,16 +1,19 @@
 package com.team18.teamproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NavUtils;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Daniel on 06/12/2015.
  */
-public class RecipeActivity extends AppCompatActivity{
+public class RecipeActivity extends AppCompatActivity {
 
     public Toolbar toolbar;
     private FragmentManager fragmentManager;
@@ -22,9 +25,9 @@ public class RecipeActivity extends AppCompatActivity{
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-       RecipeFragment recipeFragment = new RecipeFragment();
+        RecipeFragment recipeFragment = new RecipeFragment();
         fragmentManager = getSupportFragmentManager();
         setMainFragment(recipeFragment);
     }
@@ -33,5 +36,18 @@ public class RecipeActivity extends AppCompatActivity{
         fragmentManager.beginTransaction()
                 .replace(R.id.recipeFragmentContainer, fragment)
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
