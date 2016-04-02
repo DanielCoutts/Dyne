d942547533324caa1033e4104af30971d6a1713c
elasticdonut <dcoutts95@gmail.com>
2016-04-02 15:59:02 +0100

    Remove unneeded fragment

--- app/src/main/java/com/team18/teamproject/RecipeActivity.java
+++ app/src/main/java/com/team18/teamproject/RecipeActivity.java
@@ -1,10 +1,14 @@
 package com.team18.teamproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,7 +20,9 @@ import android.view.MenuItem;
 public class RecipeActivity extends AppCompatActivity {
 
     public Toolbar toolbar;
-    private FragmentManager fragmentManager;
+
+    private TabLayout tabs;
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +33,29 @@ public class RecipeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        RecipeFragment recipeFragment = new RecipeFragment();
-        fragmentManager = getSupportFragmentManager();
-        setMainFragment(recipeFragment);
-    }
+        pager = (ViewPager) findViewById(R.id.recipe_pager);
+        pager.setAdapter(new CustomAdapter(getSupportFragmentManager(), getApplicationContext()));
 
-    private void setMainFragment(Fragment fragment) {
-        fragmentManager.beginTransaction()
-                .replace(R.id.recipeFragmentContainer, fragment)
-                .commit();
+        tabs = (TabLayout) findViewById(R.id.recipe_tabs);
+        tabs.setupWithViewPager(pager);
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+        });
     }
 
     @Override
@@ -48,6 +68,39 @@ public class RecipeActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class CustomAdapter extends FragmentPagerAdapter {
+
+        private String fragments[] = {"Ingredients", "Method", "Nutrition"};
+
+        public CustomAdapter(FragmentManager fragMan, Context applicationContext) {
+            super(fragMan);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new IngredientsFragment();
+                case 1:
+                    return new MethodFragment();
+                case 2:
+                    return new NutritionFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragments[position];
         }
     }
 }
